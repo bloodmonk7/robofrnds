f29f7098238aa471e08339d555ef2d4f827ac3e3
bloodmonk7 <rritikssingh@gmail.com>
2021-06-29 14:31:48 +0530

    first commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.0conner@kory.org'
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Ambani.Mukesh@malamaal.com'
+    },
+    {
+        id: 6,
+        name: 'Ronoman Bullsy',
+        username: 'Allstar',
+        email: 'Frowny@yahoo.co.in'
+    },
+    {
+        id: 7,
+        name: 'Martha Welcrow',
+        username: 'BrownMunde',
+        email: 'Parmishverma@punjabi.com'
+    },
+    {
+        id: 8,
+        name: 'Guru Randhava',
+        username: 'Popstar',
+        email: 'Nakhra@tera.com'
+    },
+    {
+        id: 9,
+        name: 'Dhinchak Pooja',
+        username: 'Gandiawaaz',
+        email: 'dil0kashooter@merascooter.com'
+    },
+    {
+        id: 10,
+        name: 'Bantai Rapper',
+        username: 'CarryMinati',
+        email: 'Tau@youtube.com'
+    },
+]
